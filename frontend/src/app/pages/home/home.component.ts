0b916489878420d1cd250f6250a6dc30d003017b
unknown <minhkhoix01@gmail.com>
2022-09-26 18:24:10 +0700

    [update] tittle style & updating header

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,17 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  //   $(".navbar .nav-link").on('click', function(event){
+  //     var hash = this.hash;
+  //     if (this.hash !== "") {
+  //         event.preventDefault();
+  //         $('html, body').animate({
+  //             scrollTop: $(hash).offset().top
+  //         }, 1000, function(){
+  //             window.location.hash = hash;
+  //         });
+  //     } 
+  // }); 
+    //end of fx
   }
-
 }
